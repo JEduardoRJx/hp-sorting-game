214b13155f2be631c1d55eb6a1bcafa7cc562aae
Eduardo <jeduardorjx@gmail.com>
2019-10-31 22:54:15 -0600

    Add routes to my navigation images

--- src/components/navigations/Navigations.js
+++ src/components/navigations/Navigations.js
@@ -1,39 +1,49 @@
 import React from 'react';
-import './Navigations.scss'
+import { NavLink } from 'react-router-dom';
+import './Navigations.scss';
 
 const Navigations = () => {
   return (
     <div className='navigation'>
       <div className='icon-wrapper sorting-hat'>
-        <img className='main-screen-img' 
-          src={require('../../images/sortingHat.svg')} 
-          alt='sorting hat' />
-          <label>Discover Your House</label>
+        <NavLink to='/discoveryourhouse'>
+          <img className='main-screen-img' 
+            src={require('../../images/sortingHat.svg')} 
+            alt='sorting hat' />
+        </NavLink>
+        <label>Discover Your House</label>
       </div>
       <div className='icon-wrapper golden-snitch'>
-        <img className='main-screen-img' 
-          src={require('../../images/snitch.svg')} 
-          alt='golden snitch' />
-          <label>Play Game</label>
-
+        <NavLink to='/game'>
+          <img className='main-screen-img' 
+            src={require('../../images/snitch.svg')} 
+            alt='golden snitch' />
+        </NavLink>
+        <label>Play Game</label>
       </div>
       <div className='icon-wrapper instructions'>
-        <img className='main-screen-img' 
-          src={require('../../images/instructions.svg')} 
-          alt='instructions' />
-          <label>Instructions</label>
+        <NavLink to='/instructions'>
+          <img className='main-screen-img' 
+            src={require('../../images/instructions.svg')} 
+            alt='instructions' />
+        </NavLink>
+        <label>Instructions</label>
       </div>
       <div className='icon-wrapper characters'>
-        <img className='main-screen-img' 
-          src={require('../../images/characters.svg')} 
-          alt='characters' />
-          <label>View All Characters</label>
+        <NavLink to='/characters'>
+          <img className='main-screen-img' 
+            src={require('../../images/characters.svg')} 
+            alt='characters' />
+        </NavLink>
+        <label>View All Characters</label>
       </div>
       <div className='icon-wrapper wand'>
-        <img className='main-screen-img' 
-          src={require('../../images/wand.svg')} 
-          alt='wand view spells' />
-          <label>View All Spells</label>
+        <NavLink to='/spells'>
+          <img className='main-screen-img' 
+            src={require('../../images/wand.svg')} 
+            alt='wand view spells' />
+        </NavLink>
+        <label>View All Spells</label>
       </div>
     </div>
   )
