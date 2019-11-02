acb0e5c017f6a5b6db24f5be2f26ffb5402d93c1
Eduardo <jeduardorjx@gmail.com>
2019-11-01 20:06:02 -0600

    Change the game component to a class component and create a checkAnswer method

--- src/containers/game/Game.js
+++ src/containers/game/Game.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { connect } from 'react-redux';
 import './Game.scss'
+import GameChar from '../../components/gamechar/GameChar'
 
-const Game = ({ gameCharacters }) => {
-  console.log(gameCharacters)
-  return(
-    <section className='game-section'>
-      GAME SECTION
-    </section>
-  )
+class Game extends Component {
+  constructor() {
+    super();
+    this.state ={
+      index: 0,
+      correct: 0
+    }
+  }
+
+  checkAnswer = (event) => {
+    const { gameCharacters } = this.props
+    if (event.target.name === gameCharacters[this.state.index].house) {
+      console.log('correct')
+    this.setState({index: this.state.correct + 1})
+
+    } else {
+      console.log('incorrect')
+    }
+    this.setState({index: this.state.index + 1})
+  }
+
+  render() {
+    const { gameCharacters, isLoading } = this.props;
+    console.log(gameCharacters)
+    console.log(this.state)
+    
+    return (
+      <section className='game-section'>
+        <GameChar character={gameCharacters[this.state.index]} 
+          isLoading={isLoading} 
+          checkAnswer={this.checkAnswer}/>
+      </section>
+    )
+  }
 }
 
-const mapStateToProps = ({gameCharacters}) => ({
-  gameCharacters
+const mapStateToProps = ({gameCharacters, isLoading}) => ({
+  gameCharacters,
+  isLoading
 })
 
 export default connect(mapStateToProps)(Game);
