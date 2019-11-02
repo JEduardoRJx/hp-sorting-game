b2282dc00ce640b3e2dad30b9aaff64e45cf36e9
Eduardo <jeduardorjx@gmail.com>
2019-11-02 12:52:01 -0600

    Use grid to organize the characters name and house crests

--- src/components/gamechar/GameChar.js
+++ src/components/gamechar/GameChar.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import './GameChar.scss'
 
-const GameChar = ({character, isLoading, checkAnswer}) => {
+const GameChar = ({character, isLoading, checkAnswer, message, toggleModal}) => {
+  console.log(isLoading)
   if(isLoading) {
     return null;
   }
 
+  const popupMessage = <div className={`game-modal ${toggleModal}`}>
+        <h1 className='game-modal-h1'>{message}</h1>
+      </div>
+
   return (
     <article className='game-char'>
-        <h1>{character.name}</h1>
+        {popupMessage}
+        <h1 className='game-character-name'>{character.name}</h1>
         <img className='game-crest' 
           name='Gryffindor' 
           src={require('../../images/gryffindor.png')} 
+          onClick={event => checkAnswer(event)}/>
+        <img className='game-crest' 
+          name='Slytherin' 
+          src={require('../../images/slytherin.png')} 
+          onClick={event => checkAnswer(event)}/>
+        <img className='game-crest' 
+          name='Hufflepuff' 
+          src={require('../../images/hufflepuff.png')} 
+          onClick={event => checkAnswer(event)}/>
+        <img className='game-crest' 
+          name='Ravenclaw' 
+          src={require('../../images/ravenclaw.png')} 
           onClick={event => checkAnswer(event)}/>
       </article>
   )
