60fe32e645027f8b173814ad92dadf25dd04ff72
Eduardo <jeduardorjx@gmail.com>
2019-11-03 11:46:10 -0700

    Remove console logs

--- src/components/playerinfo/PlayerInfo.js
+++ src/components/playerinfo/PlayerInfo.js
@@ -3,13 +3,17 @@ import './PlayerInfo.scss'
 import { connect } from 'react-redux';
 
 const PlayerInfo = ({ user }) => {
-  console.log(user)
+  console.log(user.house)
+  const houseCrest = user.house || 'hogwarts'
   return(
     <aside>
       <img className='crest' 
-        src={require('../../images/hogwarts_crest.png')}
+        src={require(`../../images/${houseCrest}.png`)}
         alt='hogwarts crest'/>
         <h1 className='user-name'>{user.name|| '--'}</h1>
+        <h1 className='user-name'>{user.wizardkind || '--'}</h1>
+        <h1 className='user-name'>{user.wand || '--'}</h1>
+
     </aside>
   )
 }
