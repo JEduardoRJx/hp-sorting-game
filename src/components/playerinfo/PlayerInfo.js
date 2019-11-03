4a91a24adf2314436b09a16c18a3208be0e63116
Eduardo <jeduardorjx@gmail.com>
2019-11-03 11:27:52 -0700

    Import setUser indo handle user method

--- src/components/playerinfo/PlayerInfo.js
+++ src/components/playerinfo/PlayerInfo.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './PlayerInfo.scss'
+import { connect } from 'react-redux';
 
-const PlayerInfo = () => {
+const PlayerInfo = ({ user }) => {
+  console.log(user)
   return(
     <aside>
       <img className='crest' 
         src={require('../../images/hogwarts_crest.png')}
         alt='hogwarts crest'/>
+        <h1 className='user-name'>{user.name|| '--'}</h1>
     </aside>
   )
 }
 
-export default PlayerInfo
+export const mapStateToProps = ({ user }) => ({
+  user
+})
+
+export default connect(mapStateToProps)(PlayerInfo);
