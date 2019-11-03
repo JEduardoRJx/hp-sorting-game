53e3e8260d86d60cfca14d5ad417311c74061f37
Eduardo <jeduardorjx@gmail.com>
2019-11-02 21:42:57 -0600

    Setup basic DiscoverHouse class component

---
+++ src/containers/discoverhouse/DiscoverHouse.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './DiscoverHouse.scss';
+
+export class DiscoverHouse extends Component {
+  constructor() {
+    super(),
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <section className='discover-house-section'>
+        Discover House section
+      </section>
+    )
+  }
+}
+
+export default DiscoverHouse;
