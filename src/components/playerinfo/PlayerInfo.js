0d33bfb5794798a73d968353daba8ee487790a53
Eduardo <jeduardorjx@gmail.com>
2019-11-03 12:03:18 -0700

    Create variables for code readability

--- src/components/playerinfo/PlayerInfo.js
+++ src/components/playerinfo/PlayerInfo.js
@@ -3,17 +3,19 @@ import './PlayerInfo.scss'
 import { connect } from 'react-redux';
 
 const PlayerInfo = ({ user }) => {
-  console.log(user.house)
-  const houseCrest = user.house || 'hogwarts'
+  const houseCrest = user.house || 'hogwarts';
+  const userName = user.name|| 'Discover your';
+  const userWizardkind = user.wizardkind || 'House to ';
+  const userWand = user.wand || 'See Info. Here'
+
   return(
     <aside>
       <img className='crest' 
         src={require(`../../images/${houseCrest}.png`)}
         alt='hogwarts crest'/>
-        <h1 className='user-name'>{user.name|| '--'}</h1>
-        <h1 className='user-name'>{user.wizardkind || '--'}</h1>
-        <h1 className='user-name'>{user.wand || '--'}</h1>
-
+      <h1 className={`user-name ${houseCrest}`}>{userName}</h1>
+      <h2 className={`user-wizardkind ${houseCrest}`}>{userWizardkind}</h2>
+      <h2 className={`user-wand ${houseCrest}`}>{userWand}</h2>
     </aside>
   )
 }
