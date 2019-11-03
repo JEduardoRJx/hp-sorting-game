9adaeb5fd0a1d57dcb7ec980f3bdaf94bc42fb86
Eduardo <jeduardorjx@gmail.com>
2019-11-03 10:22:53 -0700

    Method to handle name and set to onChange on the input

--- src/containers/discoverhouse/DiscoverHouse.js
+++ src/containers/discoverhouse/DiscoverHouse.js
@@ -6,27 +6,49 @@ export class DiscoverHouse extends Component {
   constructor() {
     super();
     this.state = {
-
+      name: '',
+      wizardkind: '',
+      wand: '',
+      house: ''
     }
   }
 
+  handleName = (event) => {
+    this.setState({name: event.target.value})
+  }
+
+  handleWizardkindAndWand = (event) => {
+    console.log(event.target.name)
+    console.log(event.target)
+
+    this.setState({[event.target.name]: event.target.value})
+  }
+
   render() {
+    console.log(this.state)
     return (
       <section className='discover-house-section'>
         <PlayerInfo />
         <form className='house-form'>
-          <input className='name-input' type='text' placeholder='Enter Name' />
+          <input className='name-input' type='text' placeholder='Enter Name' 
+          onChange={this.handleName}/>
           <h2 className='house-label'>Witch or Wizard</h2>
           <div className='witchwizard-btn-wrapper'>
-            <button className='witchwizard-btn' type='button'>Witch</button>
-            <button className='witchwizard-btn' type='button'>Wizard</button>
+            <button className='witchwizard-btn' type='button' name='wizardkind'
+            onClick={this.handleWizardkindAndWand}>Witch</button>
+            <button className='witchwizard-btn' type='button' name='wizardkind' 
+            onClick={this.handleWizardkindAndWand}>Wizard</button>
           </div>
           <h2 className='house-label'>Select your Wand</h2>
           <div className='wand-btn-wrapper'>
-            <button className='WAND-BTN' type='button'>Elder, 15", thestral tail hair</button>
-            <button className='WAND-BTN' type='button'>Holly, 11", phoenix feather</button>
-            <button className='WAND-BTN' type='button'>Vine wood, 10 3/4", dragon heartstring</button>
-            <button className='WAND-BTN' type='button'>Fir, 9 1/2", dragon heartstring</button>
+            <button className='WAND-BTN' type='button' name='wand' 
+            onClick={this.handleWizardkindAndWand}>Elder, 15", thestral tail hair</button>
+            <button className='WAND-BTN' type='button' name='wand' 
+            onClick={this.handleWizardkindAndWand}>Holly, 11", phoenix feather</button>
+            <button className='WAND-BTN' type='button' name='wand' 
+            onClick={this.handleWizardkindAndWand}>Vine wood, 10 3/4", dragon heartstring</button>
+            <button className='WAND-BTN' type='button' name='wand' 
+            onClick={this.handleWizardkindAndWand}>Fir, 9 1/2", dragon heartstring</button>
           </div>
           <button className='your-house-btn' type='button'>Your House Is...</button>
         </form>
