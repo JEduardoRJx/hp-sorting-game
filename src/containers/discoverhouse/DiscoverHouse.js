8558f679e46db7a5450dcc21db1e114534800151
Eduardo <jeduardorjx@gmail.com>
2019-11-02 22:01:43 -0600

    Setup form

--- src/containers/discoverhouse/DiscoverHouse.js
+++ src/containers/discoverhouse/DiscoverHouse.js
@@ -14,7 +14,21 @@ export class DiscoverHouse extends Component {
     return (
       <section className='discover-house-section'>
         <PlayerInfo />
-        Discover House section
+        <form className='house-form'>
+          <input className='name-input' type='text' placeholder='Enter Name' />
+          <h2>Witch or Wizard</h2>
+          <div className='witchwizard-btn-wrapper'>
+            <button className='witch-btn' type='button'>Witch</button>
+            <button className='wizard-btn' type='button'>Wizard</button>
+          </div>
+          <div className='wand-btn-wrapper'>
+            <button className='WAND-BTN' type='button'>WAND-1</button>
+            <button className='WAND-BTN' type='button'>WAND-1</button>
+            <button className='WAND-BTN' type='button'>WAND-1</button>
+            <button className='WAND-BTN' type='button'>WAND-1</button>
+          </div>
+          <button className='your-house-btn' type='button'>Your House Is...</button>
+        </form>
       </section>
     )
   }
